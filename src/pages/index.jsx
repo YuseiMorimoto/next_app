d6197dc64e914cb45e75a3ae2267a33e1ed2840d
YuseiMorimoto <yusei0509@icloud.com>
2023-06-06 11:19:33 +0900

    ✨ useEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,25 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useEffect } from "react";
 
 export default function Home() {
-  const handleClick = (e) => {
-    console.log(e.target);
-    e.preventDefault();
-  };
+  // const handleClick = (e) => {
+  //   console.log(e.target);
+  //   e.preventDefault();
+  // };
+
+  useEffect(() => {
+    // マウント時
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      // アンマウント時
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
 
   return (
     <div className={classes.container}>
@@ -16,7 +29,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <button onClick={handleClick}>ボタン</button>
+      {/* <button onClick={handleClick}>ボタン</button> */}
 
       <Main page="index" />
 
