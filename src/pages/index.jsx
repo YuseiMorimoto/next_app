81995d51d244739ce50af63c1ac24ac29716f8cb
YuseiMorimoto <yusei0509@icloud.com>
2023-06-06 10:33:31 +0900

    🏗️ 拡張子とディレクトリ構成とインポート方法の変更。_appを用いた処理共通化をしました。

---
+++ src/pages/index.jsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import classes from "src/styles/Home.module.css";
+import { Footer } from "src/components/Footer";
+import { Main } from "src/components/Main";
+import { Header } from "src/components/Header";
+
+export default function Home() {
+  return (
+    <div className={classes.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+
+      <Main page="index" />
+
+      <Footer />
+    </div>
+  );
+}
