ee55322e93e2b265b5e9b3caa1948a29b8ef3037
YuseiMorimoto <yusei0509@icloud.com>
2023-06-06 10:57:42 +0900

    ⚗️ クリックイベントを書いてみました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,12 +5,18 @@ import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 
 export default function Home() {
+  const handleClick = (e) => {
+    console.log(e.target);
+    e.preventDefault();
+  };
+
   return (
     <div className={classes.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <button onClick={handleClick}>ボタン</button>
 
       <Main page="index" />
 
