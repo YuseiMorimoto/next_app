010f7b435006c0ef97550b5652e9c522914978d9
YuseiMorimoto <yusei0509@icloud.com>
2023-06-08 15:25:54 +0900

    ✨ useStateでstring型やboolean型、配列を扱いました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,10 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
+
   // reactのコンポーネントは状態が変化しないと再レンダリングされない
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -17,17 +21,39 @@ export default function Home() {
   const handleDisplay = useCallback(() => {
     setIsShow((prevIsShow) => !prevIsShow);
   }, []);
+
   useEffect(() => {
     // マウント時
-    console.log("foo");
     document.body.style.backgroundColor = "lightblue";
 
     return () => {
-      console.log("bar");
       // アンマウント時
       document.body.style.backgroundColor = "";
     };
   }, [count]);
+
+  // string
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      // const newArray = prevArray;
+      // newArray.push(1);
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素がすでに存在します。");
+        return prevArray;
+      }
+      const newArray = [...prevArray, text];
+      console.log(newArray === prevArray);
+      return newArray;
+    });
+  }, [text]);
 
   return (
     <div className={classes.container}>
@@ -36,12 +62,30 @@ export default function Home() {
       </Head>
       <Header />
 
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
-
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="index" />
 
       <Footer />
     </div>
   );
 }
+
+// setIsShow((isShow) => {
+//   if (isShow === true) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// });
+// }}
+
+// return isShow ? false : true;
